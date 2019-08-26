7e3c004e4a3855fd8be708ad4665fef9e45756f4
sravanvimalan <35554019+sravanvimalan@users.noreply.github.com>
2019-08-26 05:32:52 -0600

    java

---
+++ Myclass.java
@@ -0,0 +1,12 @@
+public class MyClass(int y){
+  int x;
+
+  //public MyClass(int y) {
+  //  x = y;
+  
+
+  public static void main(String[] args) {
+    MyClass myObj = new MyClass();
+    System.out.println(myObj.x);
+  }
+}
