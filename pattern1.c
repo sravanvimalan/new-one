8bd1f490c0913b3f67fae2056f59e8e90ce55174
Sravanvimalan <35554019+sravanvimalan@users.noreply.github.com>
2019-09-10 02:01:47 -0600

    Update pattern1.c

--- pattern1.c
+++ pattern1.c
@@ -24,3 +24,26 @@ int main()
 		k++;
 	}
 }
+/*
+12345
+2345
+345
+45
+5
+code of above pattern
+*/
+# include<stdio.h>
+int main()
+{
+	int n=5,k=1,r=5;
+	while(r>0)
+	{
+		for(int i=k;i<=n;i++)
+		{
+			printf("%d",i);
+		}
+		k++;
+		r--;
+		printf("\n");
+	}
+}
