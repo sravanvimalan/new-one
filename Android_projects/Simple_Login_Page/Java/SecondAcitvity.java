f773b98e3e9ff4d9594b2b95911b3c4f47a43fdd
Sravanvimalan <35554019+sravanvimalan@users.noreply.github.com>
2020-01-28 00:15:25 +0530

    Create SecondAcitvity.java

---
+++ Android_projects/Simple_Login_Page/Java/SecondAcitvity.java
@@ -0,0 +1,14 @@
+package com.example.connecti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Secondactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondactivity);
+    }
+}
