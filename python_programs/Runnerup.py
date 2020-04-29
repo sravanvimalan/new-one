0d7fed428d3cab9787642942732e56bfa661d9ca
Sravanvimalan <35554019+sravanvimalan@users.noreply.github.com>
2020-04-29 20:48:06 +0530

    Update Runnerup.py

--- python_programs/Runnerup.py
+++ python_programs/Runnerup.py
@@ -1,4 +1,4 @@
-/*
+"""
 Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given  scores.
 Store them in a list and find the score of the runner-up.
 
@@ -7,7 +7,7 @@ enter the scores: 2 3 6 6 5
 5
 
 program:
-*/
+"""
 x = input("enter the scores: ")
 x = x.split()
 x = list(set(x))  #set has has no duplicate elements
