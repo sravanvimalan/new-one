bb23fe51a74ca1618a907abd665c69a000fb7a0b
Sravanvimalan <35554019+sravanvimalan@users.noreply.github.com>
2020-05-20 16:33:42 +0530

    Update Myclass.java

--- Udemy/Android/GuessGame/Myclass.java
+++ Udemy/Android/GuessGame/Myclass.java
@@ -48,54 +48,64 @@ Guess the number?
 12
 You win
 
-*/
-package com.sravan;
-import java.util.Scanner;
-import java.util.Random;
-class Myclass
-{
-
-	public static void main(String[] args)
+*/package com.sravan;
+	import java.util.Scanner;
+	import java.util.Random;
+	class Myclass
 	{
-		String name;
-		int i=5,rand_no;
-		int start;
-		Scanner sc = new Scanner(System.in);
-		Random rand = new Random();
-		System.out.println("Welcome to Guess Number Game...wish you good luck\n");
-		System.out.println("Do I know your name?\n");
-		name = sc.next();
-		System.out.println("Hello "+name+"\n");
-		System.out.println("shall we start the game ?\npress 1: start\npress 0: exit\n");
-		start = sc.nextInt();
-		if(start == 1)
-			{
-				System.out.println("ok game start..you have only 5 chances for guess! && guess value 1<=x<=20\n");
-				rand_no = rand.nextInt(20+1);
-				while(i > 0)
+	
+		public static void main(String[] args)
+		{
+			String name;
+			int i=5,rand_no;
+			int start;
+			Scanner sc = new Scanner(System.in);
+			Random rand = new Random();
+			System.out.println("Welcome to Guess Number...wish you good luck\n");
+			System.out.println("Do I know your name?\n");
+			name = sc.next();
+			System.out.println("Hello "+name+"\n");
+			System.out.println("shall we start the game ?\npress 1: start\npress 0: exit\n");
+			start = sc.nextInt();
+			if(start == 1)
 				{
-					int no;
-					System.out.println("Guess the number?\n");
-					no = sc.nextInt();
-					if(no == rand_no)
-						System.out.println("You win\n");
-					else
+					System.out.println("ok game start..you have only 5 chances for guess! && guess value 1<=x<=20\n");
+					rand_no = rand.nextInt(20+1);
+					while(i > 0)
 					{
-						System.out.println("Wrong "+(i-1)+" chances left\n");
-						if(no > rand_no)
-							System.out.println("Guess Lower\n");
+						int no;
+						System.out.println("Guess the number?\n");
+						no = sc.nextInt();
+						if(no == rand_no)
+						{
+							System.out.println("You win\n");
+							sc.close();
+							break;
+						}
 						else
-							System.out.println("Guess Higher\n");
-					}	
-					i--;
-				
+						{
+							System.out.println("Wrong "+(i-1)+" chances left\n");
+							if(i == 1)
+							{
+								sc.close();
+								System.out.println("Game Over");
+								break;
+							}
+							if(no > rand_no)
+								System.out.println("Guess Lower\n");
+							else
+								System.out.println("Guess Higher\n");
+						}	
+						i--;
+					
+					}
+					
 				}
-				sc.close();
-			}
-		else
-			{
-				System.out.println("Exit successfuly\n");
-			}
-				sc.close();
+			else
+				{
+					System.out.println("Exit successfuly\n");
+					sc.close();
+					return;
+				}
 		}
-}
+	}
