60ab5e5651188d294521dce5d10c82a2ee709e30
Sravanvimalan <35554019+sravanvimalan@users.noreply.github.com>
2020-05-22 11:25:44 +0530

    Create Phone.java

---
+++ Udemy/Android/OOP Concept/Phone.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Phone {
+    private String Name;
+    private String MemoryRam;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getMemoryRam() {
+        return MemoryRam;
+    }
+
+    public void setMemoryRam(String memoryRam) {
+        MemoryRam = memoryRam;
+    }
+}
