3735087e7aaf54b6d85bfabeaefc3de024c545b9
Sravanvimalan <35554019+sravanvimalan@users.noreply.github.com>
2020-05-25 22:46:18 +0530

    Create Server.java

---
+++ Java_programs/Socket/Server.java
@@ -0,0 +1,34 @@
+/*
+ouput:
+Waiting for clients
+Connection Established
+
+program:
+*/
+package com.company;
+
+import java.io.BufferedReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+    //server socket
+            try {
+                System.out.println("Waiting for clients");
+                ServerSocket serverSocket = new ServerSocket(5000);
+                Socket socket = serverSocket.accept();
+                System.out.println("Connection Established");
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String str = bufferedReader.readLine();
+                PrintWriter printWriter = new PrintWriter(socket.getOutputStream(),true);
+                printWriter.println(str);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+
+    }
+}
