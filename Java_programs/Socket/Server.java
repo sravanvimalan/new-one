8034f2312c21d2a79cbc5141f1ae5de2586c736b
Sravanvimalan <35554019+sravanvimalan@users.noreply.github.com>
2020-05-25 22:46:37 +0530

    Update Server.java

--- Java_programs/Socket/Server.java
+++ Java_programs/Socket/Server.java
@@ -14,7 +14,7 @@ import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void Server(String[] args) {
     //server socket
             try {
                 System.out.println("Waiting for clients");
