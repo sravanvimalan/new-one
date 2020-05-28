d41123d33f60063fb3bc45374a5e78c446a747b7
Sravanvimalan <35554019+sravanvimalan@users.noreply.github.com>
2020-05-28 17:23:52 +0530

    Create Target_sum_subarray.py

---
+++ python_programs/Target_sum_subarray.py
@@ -0,0 +1,26 @@
+/*
+output:
+[ 5 7 2 ]
+program:
+*/
+
+
+
+list = [1, 3, 2, 5, 7, 2]
+result_sum = 0
+target_sum = 14
+start = 0
+for i in range(len(list)) :
+        while(result_sum >= target_sum):
+            result_sum=result_sum-list[start]
+            start=start+1
+        result_sum=result_sum+list[i]
+if result_sum == target_sum:
+    print("[",end=' ')
+    for i in range(start,len(list)):
+        print(list[i],end=' ')
+    print("]")
+
+
+
+
