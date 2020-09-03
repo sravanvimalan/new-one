ca252a603db4061048f23bec8d212d1816c1755d
Sravanvimalan <35554019+sravanvimalan@users.noreply.github.com>
2020-09-03 17:25:22 +0530

    Update Printing_Longest_Common_Subsequence.java

--- Dynamic Programming/Printing_Longest_Common_Subsequence.java
+++ Dynamic Programming/Printing_Longest_Common_Subsequence.java
@@ -43,13 +43,8 @@ public class Printing_Longest_Common_Subsequence {
 				}
 			}
 		}
-		for(int i=0;i<len1;i++)
-		{
-			for(int j=0;j<len2;j++)
-			{
-				System.out.print(arr[i][j]);
-			}
-			System.out.print("\n");
-		}
+				
+		System.out.print(arr[len1-1][len2-1]);      //return length
+			
 	}
 }
