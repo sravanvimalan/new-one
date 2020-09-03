3606163b1c761daa65edae471db7d4e8db148f2d
Sravanvimalan <35554019+sravanvimalan@users.noreply.github.com>
2020-09-03 17:15:19 +0530

    Create Printing_Longest_Common_Subsequence.java

---
+++ Dynamic Programming/Printing_Longest_Common_Subsequence.java
@@ -0,0 +1,55 @@
+package learn;
+
+public class Printing_Longest_Common_Subsequence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String firstString="gxtxayb";
+		String secondString="aggtab";
+		int len1=firstString.length();
+		int len2=secondString.length();
+		len1++; //extra row
+		len2++; //extra col
+		int[][] arr = new int[len1][len2];
+		
+		//fill the first row and column with zeros
+		
+		for(int i=0;i<len1;i++)
+		{
+			for(int j=0;j<len2;j++)
+			{
+				arr[i][j]=0;
+			}
+		}
+		for(int i=1;i<len1;i++)
+		{
+		   for(int j=1;j<len2;j++)
+			{
+				if(firstString.charAt(i-1) == secondString.charAt(j-1))
+				{
+					arr[i][j]=arr[i-1][j-1]+1;
+				}
+				else
+				{
+					if(arr[i][j-1] > arr[i-1][j] )
+					{
+						arr[i][j]=arr[i][j-1];
+					}
+					else
+					{
+						arr[i][j]=arr[i-1][j];
+					}
+				}
+			}
+		}
+		for(int i=0;i<len1;i++)
+		{
+			for(int j=0;j<len2;j++)
+			{
+				System.out.print(arr[i][j]);
+			}
+			System.out.print("\n");
+		}
+	}
+}
