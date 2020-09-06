032e2227ee48965a9cbb776ed7d4b7f73d123de5
Sravanvimalan <35554019+sravanvimalan@users.noreply.github.com>
2020-09-06 12:11:54 +0530

    Update Binomial_Coefficient.java

--- Java_programs/Binomial_Coefficient.java
+++ Java_programs/Binomial_Coefficient.java
@@ -2,18 +2,16 @@ package learn;
 
 public class Binomial_Coefficient {
   
-	 static int count=0;
+	
 	 public static int c(int n,int x)
 	 {
 		if(x==0)
-		{
-			count++;
-			return count;
+		{	
+			return 1;
 		}
 		if(n==x)
-		{
-			count++;
-			return count;
+		{	
+			return 1;
 		}
 		return c(n-1,x-1)+c(n-1,x);
 			
@@ -23,8 +21,8 @@ public class Binomial_Coefficient {
 		// TODO Auto-generated method stub
 		//read value for n and x   "nCx"
 		int n=10,x=7;
-		c(n,x);
-		System.out.println(count);
+		
+		System.out.println(c(n,x));
 	}
 
 }
